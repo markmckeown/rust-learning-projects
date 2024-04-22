53de19c2b19f45d859ad9cacbf9027860c4c9246
Mark Mc Keown <mark.mckeown@wandisco.com>
2024-04-22 09:14:08 +0100

    Vectorsgit status

---
+++ vectors/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let mut v = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v[2];
+    println!("The third element is {third}");
+
+    let third: Option<&i32> = v.get(2);
+    match third {
+        Some(third) => println!("The third element is {third}"),
+        None => println!("There is no third element."),
+    }
+
+    for i in &mut v {
+        *i += 50;
+        println!("{i}");
+    }
+
+}
